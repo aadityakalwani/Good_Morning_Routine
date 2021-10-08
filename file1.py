89fe3aa10da4bd1cf30f4784750b33fea0f9ac39
aadityakalwani <aadityakalwani@gmail.com>
2021-10-08 13:42:40 +0100

    completed open_spotify def

--- file1.py
+++ file1.py
@@ -10,9 +10,14 @@ import pyautogui
 
 def open_spotify():
 
+    # this part would open spotify itself using the raycast shortcut
     pyautogui.hotkey("command", "shift", "s", interval=0.1)
-    time.sleep(2)
+    time.sleep(5)
     print("spotify is now open")
+
+    # this part would full screen
+    pyautogui.hotkey("control", "command", "f", interval=0.1)
+    time.sleep(3)
 
 
 open_spotify()
